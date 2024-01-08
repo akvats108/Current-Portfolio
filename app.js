67a93113e88873df8e4535deec84ff71dd06412e
akvats108 <115577645+akvats108@users.noreply.github.com>
2024-01-08 12:33:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,16 +5,23 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('.main'),
     smooth: true
 });
-
+ 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 10,
+    cssMode: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+       
+    },
     pagination: {
       el: ".swiper-pagination",
-      clickable: true,
-
     },
+    mousewheel: true,
+    keyboard: true,
+    
   });
+
+  
 
 
 // calculator code here
